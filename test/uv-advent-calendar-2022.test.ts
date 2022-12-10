49e28a8cb2ec03d2c82a3bc8846b329ac925a7aa
glaceef <glaceef@gmail.com>
2022-12-10 19:06:20 +0900

    Initial commit

---
+++ test/uv-advent-calendar-2022.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UvAdventCalendar2022 from '../lib/uv-advent-calendar-2022-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/uv-advent-calendar-2022-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UvAdventCalendar2022.UvAdventCalendar2022Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
